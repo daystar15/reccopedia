960480b97bf2cde08db372f5d16bb54f5d9e095a
test <test@test.com>
2023-02-08 18:43:46 +0900

    bo가 dao 부르도록 설정중(api)

--- src/main/java/com/reccopedia/restAPI/bo/RestAPIBO.java
+++ src/main/java/com/reccopedia/restAPI/bo/RestAPIBO.java
@@ -1,5 +1,11 @@
 package com.reccopedia.restAPI.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.reccopedia.restAPI.dao.RestAPIDAO;
+
 public class RestAPIBO {
 
+	@Autowired
+	private RestAPIDAO restapiDAO;
 }
