2e1d9606ef0656cacb854a4d62239b41cc25fae9
daystar_15 <daystar_15@naver.com>
2023-02-10 04:31:22 +0900

    외부 api 불러오는 중(수요일이랑 같은 상태, 학원에서 구동됐을 때랑 동일한 상태로 다시 돌려놨는데 계속 오류 발생함, 일단 커밋)

--- src/main/java/com/reccopedia/contents/ContentsController.java
+++ src/main/java/com/reccopedia/contents/ContentsController.java
@@ -1,17 +1,13 @@
 package com.reccopedia.contents;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.reccopedia.restAPI.bo.MainBO;
-
 @Controller
 public class ContentsController {
 
-	@Autowired
-	private MainBO mainBO;
+
 	
 	// 메인 페이지
 	@GetMapping("/main")
--- src/main/java/com/reccopedia/contents/ContentsRestController.java
+++ src/main/java/com/reccopedia/contents/ContentsRestController.java
@@ -1,29 +1,10 @@
 package com.reccopedia.contents;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.reccopedia.restAPI.bo.MainBO;
 
 @RestController
 public class ContentsRestController {
 
-	@Autowired
-	private MainBO restapiBO;
 	
-	// 제목, 년도, 나라, 평균별점, 예매율, ~만명
-	//@GetMapping("/main")
-	public Map<String, Object> main() {
-		
-		Map<String, Object> result = new HashMap<>();
-		
-		
-		
-		
-		return result;
-	}
 	
 }
